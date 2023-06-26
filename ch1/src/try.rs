a8fe5ddd04daead92e495c8376ddffc35ddb27f6
KNyathi <khayelihlenyathi07@gmail.com>
2023-06-26 13:52:04 +0300

    change ch1

--- ch1/src/try.rs
+++ ch1/src/try.rs
@@ -1,6 +1,15 @@
 fn main() {
-    println!("Hello, we are in try1 now!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
     }
+
+    println!("The value of x is: {x}");
+}
 
 
 
