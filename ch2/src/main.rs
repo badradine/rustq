d20da2d31e5b6fc61b0497dd1c05b68b55f65da4
badradine <bodourali235@gmail.com>
2023-07-11 12:56:42 +0300

    Add CI/CD

--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -18,5 +18,5 @@ fn main() {
 
     let element = a[index];
 
-    println!("The value of the element at index {index} is: {element}");
+    println!("The value of the element at this index {index} is: {element}");
 }
